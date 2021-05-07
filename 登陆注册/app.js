5f1f4f9a82485ae84de60fdab8694dd9c7d1bc87
HXY <1969574105@qq.com>
2021-05-07 11:27:15 +0800

    创建电子图书馆项目

---
+++ 登陆注册/app.js
@@ -0,0 +1,28 @@
+var express = require('express')
+var router = require('./router.js')
+var bodyParser = require('body-parser')
+var session = require('express-session')
+
+var app = express()
+
+app.engine('html', require('express-art-template'))
+
+// 配置
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }))
+
+app.use('/public/', express.static('./public/'))
+app.use('/static/', express.static('./static/'))
+app.use('/views/', express.static('./views/'))
+app.use(router)
+
+
+app.listen(8090, function() {
+    console.log('app is running at port 3000')
+})
